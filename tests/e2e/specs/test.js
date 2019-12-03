04786f856c14341a4df91e8946c69691eb33fc03
Encaik <843391622@qq.com>
2019-12-03 13:53:07 +0800

    修改代码

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,21 +1,33 @@
-// For authoring Nightwatch tests, see
-// https://nightwatchjs.org/guide
-
 module.exports = {
-  "default e2e tests": browser => {
+  "Demo test baidu": function(browser) {
     browser
-      .init()
-      .waitForElementVisible("#app")
-      .assert.elementPresent(".hello")
-      .assert.containsText("h1", "Welcome to Your Vue.js App")
-      .assert.elementCount("img", 1)
-      .end();
-  },
-
-  "example e2e test using a custom command": browser => {
-    browser
-      .openHomepage()
-      .assert.elementPresent(".hello")
+      .url("https://www.baidu.com/")
+      .waitForElementVisible("body")
+      .assert.titleContains("百度一下，你就知道")
+      .assert.visible("#kw")
+      .setValue("#kw", "vue")
+      .assert.visible("input[type=submit]")
+      .click("input[type=submit]")
+      .assert.containsText("#content_left", "Vue.js")
+      .click(".opr-recommends-merge-mask")
+      .assert.containsText("#content_left", "Bootstrap")
       .end();
   }
+  // "@disabled": false,
+  // "default e2e tests": browser => {
+  //   browser
+  //     .init()
+  //     .waitForElementVisible("#app")
+  //     .assert.elementPresent(".hello")
+  //     .assert.containsText("h1", "Welcome to Your Vue.js App")
+  //     .assert.elementCount("img", 1)
+  //     .end();
+  // }
+
+  // "example e2e test using a custom command": browser => {
+  //   browser
+  //     .openHomepage()
+  //     .assert.elementPresent(".hello")
+  //     .end();
+  // }
 };
